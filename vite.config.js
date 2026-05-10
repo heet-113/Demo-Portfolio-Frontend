72e1d1716056686e075fa42889f77de18023b0ca
heet-113 <heetr102@gmail.com>
2026-05-10 13:33:44 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Use repo-based base path on GitHub Pages project sites, root on user/org sites.
+  base: (() => {
+    const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
+    if (!repoName || repoName.endsWith('.github.io')) {
+      return '/'
+    }
+    return `/${repoName}/`
+  })(),
 })
